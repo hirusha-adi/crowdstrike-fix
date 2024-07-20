05f31bebf2e8a80a19386fcb6869bd98850d6463
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2024-07-20 23:06:20 +0530

    delete files matching pattern

--- fix.ps1
+++ fix.ps1
@@ -4,8 +4,27 @@ $path = Join-Path -Path $env:WINDIR -ChildPath "System32\drivers\CrowdStrike"
 if (Test-Path -Path $path) {
     Write-Output "Found CrowdStrike folder in $path"
     
-    # ls
+    # ls: before
     Get-ChildItem -Path $path
+
+    # Find and delete all files matching C-00000291*.sys
+    # ---
+    $filesToDelete = Get-ChildItem -Path $path -Filter "C-00000291*.sys"
+    
+    foreach ($file in $filesToDelete) {
+        try {
+            Write-Output "Deleting: $($file.FullName)"
+            Remove-Item -Path $file.FullName -Force
+        }
+        catch {
+            Write-Output "Failed to delete file: $($file.FullName) - $($_.Exception.Message)"
+        }
+    }
+    # ---
+
+    # ls: after
+    Get-ChildItem -Path $path
+
 } else {
     Write-Output "CrowdStrike folder not found in $($env:WINDIR)\System32\drivers"
 }
